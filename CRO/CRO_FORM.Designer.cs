a3e608a4c578f2f5ab445f5c2513db9590c88253
Matej <b-matt@net.hr>
2016-07-09 11:35:35 +0200

    Add files via upload

---
+++ CRO/CRO_FORM.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace CRO
+{
+    partial class CRO_FORM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CRO_FORM));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 126);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(143, 17);
+            this.toolStripStatusLabel1.Text = "    Napravio: Matej Arlović";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 24);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(192, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Brzina komentiranja(što sporije to bolje):";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(80, 51);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(135, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // CRO_FORM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 148);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CRO_FORM";
+            this.Text = "#CROmentator";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.CRO_FORM_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ CRO/CRO_FORM.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace CRO
+{
+    public partial class CRO_FORM : Form
+    {
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, IntPtr ProcessId);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr AttachThreadInput(IntPtr idAttach,
+                      IntPtr idAttachTo, bool fAttach);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetFocus();
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowTextLength", SetLastError = true)]
+        internal static extern int GetWindowTextLength(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint wMsg, UIntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint wMsg, UIntPtr wParam, [MarshalAs(UnmanagedType.LPStr)] string lParam);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern bool PostMessage(IntPtr hWnd, uint Msg, UIntPtr wParam, UIntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern bool PostMessage(IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam);
+
+        private const uint EM_GETSEL = 0xB0;
+        private const uint EM_SETSEL = 0x00B1;
+        private const uint EM_REPLACESEL = 0x00C2;
+        private const int WM_KEYDOWN = 0x100;
+        private const int WM_KEYUP = 0x101;
+        private const int VK_RETURN = 0x0D;
+
+        private bool IsPosting = false;
+
+        public CRO_FORM()
+        {
+            InitializeComponent();
+        }
+
+        private IntPtr FocusedWindows()
+        {
+            IntPtr activeWindowHandle = GetForegroundWindow();
+            IntPtr activeWindowThread = GetWindowThreadProcessId(activeWindowHandle, IntPtr.Zero);
+            IntPtr thisWindowThread = GetWindowThreadProcessId(this.Handle, IntPtr.Zero);
+
+            AttachThreadInput(activeWindowThread, thisWindowThread, true);
+            IntPtr focusedHandle = GetFocus();
+            AttachThreadInput(activeWindowThread, thisWindowThread, false);
+            return focusedHandle;
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            IntPtr WindowsHandle = FocusedWindows();
+            int StartPos = 0, EndPos = 0;
+
+            string hashText = "#CRO #OrangeSponsorsYou";
+             foreach (char character in hashText) {
+                SendMessage(WindowsHandle, EM_GETSEL, (UIntPtr)StartPos, (IntPtr)EndPos);
+                SendMessage(WindowsHandle, EM_SETSEL, (UIntPtr)StartPos, (IntPtr)EndPos);
+                PostMessage(WindowsHandle, 0x0102, (UIntPtr)(character), UIntPtr.Zero);
+            }
+
+            SendMessage(WindowsHandle, EM_GETSEL, (UIntPtr)StartPos, (IntPtr)EndPos);
+            SendMessage(WindowsHandle, EM_SETSEL, (UIntPtr)StartPos, (IntPtr)EndPos);
+            PostMessage(WindowsHandle, WM_KEYDOWN, (UIntPtr)VK_RETURN, UIntPtr.Zero);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if( !IsPosting ) {
+                timer1.Interval = (int)numericUpDown1.Value * 1000;
+                timer1.Enabled = true;
+                button1.Text = "Stop";
+                IsPosting = true;
+            } else {
+                timer1.Enabled = false;
+                button1.Text = "Start";
+                IsPosting = false;
+            }
+        }
+
+        private void CRO_FORM_Load(object sender, EventArgs e)
+        {
+            button1.Text = "Start";
+            IsPosting = false;
+            timer1.Enabled = false;
+        }
+    }
+}
